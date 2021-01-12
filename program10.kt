4d79738d87ba44e3fb1a7a979ca28a815e0a9edc
Niyati Sinha <niyati.sinha2999@gmail.com>
2021-01-12 11:53:43 +0530

    Add files via upload

---
+++ program10.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    println("Kotlin Supports type inferences on variables and properties like Python")
+    println("It means that Kotlin can recognize what kind of a value we are trying to assign to a variable")
+    println("Thus, we can actually omit a type declaration of the variables")
+    var FirstName : String = "Niyati" //Way1
+    var LastName = "Sinha" //Way2
+    println(FirstName+" "+ LastName)
+    println("Although we cannot store null to the variable LastName as by default variables are non-null type in Kotlin")
+    //LastName = null //Error: Kotlin: Null can not be a value of a non-null type String
+}
--- program9.kt
+++ program9.kt
@@ -7,10 +7,28 @@ fun main()
     print("a is : ")
     println(a)
 
-    //val b: String= null // Error: Kotlin: Null can not be a value of a non-null type String
+    //var b: String= null or val b: String= null// Error: Kotlin: Null can not be a value of a non-null type String
     //This error occurs as we have defined a String b which can store non-null strings
-    //In order to define strings that can store null we have to give a ? just after the data type String
+    //In order to define strings that can store null as well as non null we have to give a ? just after the data type String
 
-    val c: String? = null
-    println(c)
+    var c: String? = null
+    println("c is: "+c)
+    c = "Hi!"
+    println("Now c is: "+c)
+    c=null
+    println("Now c is again: "+c)
+
+    val d: String? = null
+    println("d is: "+d)
+
+    val e: String? = "We can store non-null values as well!"
+    println("e is: "+e)
+
+    var f: String? = "non-null"
+    println("f is: "+f)
+    f = null
+    println("Now f is: "+f)
+    f = "not a null string"
+    println("Now f is again: "+f)
+
 }
